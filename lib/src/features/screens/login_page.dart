7af6e8a7b60efc7a49b94c20f57348dc1b14f91d
dillahmwangi <mcharity241@gmail.com>
2024-05-30 12:55:47 +0300

    made changes on the on theme color and created login page

---
+++ lib/src/features/screens/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: const Column(
+        children: [
+          //logo
+
+          //Welcome back message
+
+          //email textfield 
+
+          //password textfield
+
+          //register now
+        ],
+      ),
+
+    );
+  }
+}
