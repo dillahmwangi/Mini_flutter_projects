a1d48ee7ddce8e0b2ef8490efb8c565984cd34ec
dillahmwangi <mcharity241@gmail.com>
2024-06-05 09:22:00 +0300

    changed button color

--- lib/src/components/my_button.dart
+++ lib/src/components/my_button.dart
@@ -4,7 +4,7 @@ class MyButton extends StatelessWidget {
   final void Function()? onTap;
   final String text;
 
-   const MyButton({
+  const MyButton({
     super.key,
     required this.text,
     required this.onTap,
@@ -15,16 +15,18 @@ class MyButton extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      
-      padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.symmetric(horizontal: 26),
-      child: Center(
-        child: Text(text),
-      ),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.outline,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.symmetric(horizontal: 26),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ),
       ),
     );
   }
